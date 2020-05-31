af407faff272e17a568b0100b9b6deb7e3d67744
LeoCR <leonardoas2007@gmail.com>
2020-05-30 20:03:51 -0600

    Using String Interpolation DataBinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-
+  serverId:number=10;
+  serverStatus:string='offline';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
