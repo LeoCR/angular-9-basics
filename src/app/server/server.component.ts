6860bf83b6032de149d2101a4655ae7178cfba25
LeoCR <leonardoas2007@gmail.com>
2020-06-02 17:29:21 -0600

    Outputting Lists with ngFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -12,6 +12,7 @@ export class ServerComponent implements OnInit {
   serverCreationStatus:string="No server was created!";
   serverName:string='Test Server';
   serverCreated:boolean=false;
+  servers:string[]=['Server 01','Server 02'];
   constructor() {
       setTimeout(() => {
           this.allowNewServer=true;
@@ -25,6 +26,7 @@ export class ServerComponent implements OnInit {
     return this.serverStatus;
   }
   onCreateServer(){
+    this.servers.push(this.serverName);
     this.serverCreated=true;
     this.serverCreationStatus="Server was created! Name is: "+this.serverName;
   }
